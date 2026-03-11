edfc4d14e9dd5f163d11b5959fd199d5b1db69b9
Lee Gyungmin <rudals831@gmail.com>
2026-03-11 16:47:56 +0900

    initialize

---
+++ src/main/java/com/example/sum_gil_be/SumGilApplication.java
@@ -0,0 +1,13 @@
+package com.example.sum_gil_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumGilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumGilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sum_gil_be/SumGilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sum_gil_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumGilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
